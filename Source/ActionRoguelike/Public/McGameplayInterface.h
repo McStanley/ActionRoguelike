494cc22d39cf4982c0cc4e8efe8895a8ccee4035
Stanisław Olejniczak <0stanislawolejniczak0@gmail.com>
2024-07-10 02:30:06 +0200

    Create lever

--- Source/ActionRoguelike/Public/McGameplayInterface.h
+++ Source/ActionRoguelike/Public/McGameplayInterface.h
@@ -22,6 +22,6 @@ class ACTIONROGUELIKE_API IMcGameplayInterface
 
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
-	UFUNCTION(BlueprintNativeEvent)
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
 	void Interact(APawn* InstigatorPawn);
 };
