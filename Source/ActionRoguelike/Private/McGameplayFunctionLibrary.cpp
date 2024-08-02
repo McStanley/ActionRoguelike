a691862470744959418910c1c05251a8ab6b4940
Stanisław Olejniczak <0stanislawolejniczak0@gmail.com>
2024-08-02 13:30:03 +0200

    Fix impulse direction in ApplyDirectionalDamage

--- Source/ActionRoguelike/Private/McGameplayFunctionLibrary.cpp
+++ Source/ActionRoguelike/Private/McGameplayFunctionLibrary.cpp
@@ -21,8 +21,10 @@ bool UMcGameplayFunctionLibrary::ApplyDirectionalDamage(AActor* InstigatorActor,
 		UPrimitiveComponent* HitComp = HitResult.GetComponent();
 		if (HitComp && HitComp->IsSimulatingPhysics(HitResult.BoneName))
 		{
-			HitComp->AddImpulseAtLocation(-HitResult.ImpactNormal * 300000.f, HitResult.ImpactPoint,
-			                              HitResult.BoneName);
+			FVector Direction = HitResult.TraceEnd - HitResult.TraceStart;
+			Direction.Normalize();
+
+			HitComp->AddImpulseAtLocation(Direction * 300000.f, HitResult.ImpactPoint, HitResult.BoneName);
 		}
 
 		return true;
